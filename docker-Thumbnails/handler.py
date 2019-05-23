ceaf5ebe086886b8e7e60ffe9921ed78403a6389
Rufino Rojas <lucras015@hotmail.com>
2019-05-23 14:56:49 -0500

    se agrego thumbnails

--- docker-Thumbnails/handler.py
+++ docker-Thumbnails/handler.py
@@ -1,24 +1,68 @@
 import json
+import boto3
+import cStringIO
+from PIL import Image, ImageOps
+import os
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+s3 = boto3.client('s3')
+size = int(os.environ['Image_size'])
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def generate_image_s3(event, context):
+    print(event)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    image = get_s3_image(bucket, key)
+
+    thumb = image_to_thumb(image)
+    thumb_key = new_filename(key)
+
+    url = upload_to_s3(bucket, thumb_key, thumb)
+
+    return url
+
+
+def get_s3_image(bucket, key):
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+    imagecontent = response['body'].read()
+
+    file = cStringIO.StringIO(imagecontent)
+    img = Image.open(file)
+
+    return img
+
+
+def image_to_thumb(image):
+    return ImageOps.fit(image, (size, size), Image.ANTIALIAS)
+
+
+def new_filename(key):
+    key_split = key.rsplit('.', 1)
+    return key_split + "_thumbnail.png"
+
+
+def upload_to_s3(bucket, key, image):
+
+    out_thumb = cStringIO.StringIO()
+
+    image.save(out_thumb, 'PNG')
+
+    out_thumb.seek(0)
+
+    response = s3.put_object(
+        ACL='public-read',
+        Body=out_thumb,
+        Bucket=bucket,
+        ContentType='image/png',
+        Key=key
+    )
+
+    print(response)
+
+    url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+
+    return url
