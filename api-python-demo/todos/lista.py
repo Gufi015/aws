e091f0c2f2980c2908f3f70fc032502e7276ff8d
Rufino Rojas <lucras015@hotmail.com>
2019-05-24 17:00:44 -0500

    se agrego list

--- api-python-demo/todos/lista.py
+++ api-python-demo/todos/lista.py
@@ -3,16 +3,16 @@ import os
 
 from todos import decimalencoder
 
-def lambda_handler(event, context):
+def lista(event, context):
     dynamodb = boto3.resource('dynamodb')
 
-    table = boto3.resource('dynamodb').Table('python')
+    table = boto3.resource('dynamodb').Table('api-python-demo-dev')
     result = table.scan()
 
-    # TODO implement
-    response = {
-        'statusCode': 200,
-        'body': json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
-    }
+    response = json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
 
-    return response
+    data = json.loads(response)
+    print(data)
+
+    # TODO implement
+    return data
