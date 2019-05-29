de152353264102caeefb0f6ae057ba2dbebcaa3d
Rufino Rojas <lucras015@hotmail.com>
2019-05-29 14:23:32 -0500

    add exports get to get id from database dynamodb table employees

---
+++ employee/employees/employee/get.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+
+
+module.exports.get = (event, context, callback)=>{
+
+
+    const params = {
+        TableName: process.env.employees,
+        Key:{
+            id: event.pathParameters.id
+        }
+    };
+
+
+
+    dynamodb.get(params, (error, result)=>{
+
+        if(error){
+            console.log(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers:{'Content-Type':'application/json'},
+                body: 'don`t get id item of database dynamodb',
+            });
+
+            return;
+        }
+
+        const response ={
+            statusCode: 200,
+            body: JSON.stringify(result.Item)
+        };
+
+        callback(null, response);
+    });
+};
